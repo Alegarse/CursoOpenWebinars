8cbfdbc80afec8fd5aed3202fd156b0045533259
Fran López <franlopjur@gmail.com>
2018-02-16 09:35:43 +0100

    Update CarModel

--- Seccion4AndroidMeetsKotlin/Leccion8BasesDeDatos/app/src/main/java/com/openwebinars/mydatabase/model/CarModel.kt
+++ Seccion4AndroidMeetsKotlin/Leccion8BasesDeDatos/app/src/main/java/com/openwebinars/mydatabase/model/CarModel.kt
@@ -5,5 +5,5 @@ import io.realm.annotations.PrimaryKey
 import io.realm.annotations.RealmClass
 
 @RealmClass
-class CarModel(@PrimaryKey var id: String = "", var brand: String = "") : RealmObject()
+open class CarModel(@PrimaryKey var id: String = "", var brand: String = "") : RealmObject()
 
